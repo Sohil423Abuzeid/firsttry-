0c7798f1a5a05006cca86adcc66dd31e441d18f5
sohail-abozed <sohail-abozed@SOHAIL>
2022-04-22 16:32:57 +0200

    Add project files.

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,19 @@
+
+
+#include <iostream>
+using namespace std;
+static int calc(int a , int b)
+{
+    int s = a + b;
+    return s;
+}
+int main()
+{
+    int a,b;
+    cin >> a;
+    cin >> b;
+    cout << calc(a,b);
+    
+    
+}
+
